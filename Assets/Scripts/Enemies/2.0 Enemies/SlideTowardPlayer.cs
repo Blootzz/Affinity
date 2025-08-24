c248e2e63f6a9de36acc06999e1723cc0f30e3c7
Blootzz <106506598+Blootzz@users.noreply.github.com>
2025-08-24 17:17:01 -0500

    FIXED lerp to player (apply root motion)

--- Assets/Scripts/Enemies/2.0 Enemies/SlideTowardPlayer.cs
+++ Assets/Scripts/Enemies/2.0 Enemies/SlideTowardPlayer.cs
@@ -2,27 +2,24 @@ using UnityEngine;
 
 public class SlideTowardPlayer : MonoBehaviour
 {
-    [SerializeField] PhysicsMaterial2D zeroFrictionBounce;
-    [SerializeField] PhysicsMaterial2D slideMaterial;
-
     Rigidbody2D rb;
     Transform playerTransform;
     Vector2 resultTargetPos;
     float targetXPos;
     float lerpStrength;
-    [SerializeField] float baseLerpMultiplier = 10;
+    [SerializeField] float baseLerpMultiplier = 1;
 
     bool slidingActive = false;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        if (GetComponent<Animator>().applyRootMotion == false)
+            Debug.LogWarning("Animator.applyRootMotion must be true or it will override any change in position. 2 hours lost to this quirk");
     }
 
     public void BeginSlide(Transform pTransform, float strength)
     {
-        rb.sharedMaterial = slideMaterial;
-
         playerTransform = pTransform;
         lerpStrength = strength;
 
@@ -31,22 +28,21 @@ public class SlideTowardPlayer : MonoBehaviour
     public void EndSlide()
     {
         slidingActive = false;
-        rb.sharedMaterial = zeroFrictionBounce;
     }
 
-    void Update()
+    // hopefully having rb.interpolate allows smooth movement
+    void FixedUpdate()
     {
         if (!slidingActive)
             return;
 
         // lerp from here to player by lerpStrength
-        targetXPos = Mathf.Lerp(transform.position.x, playerTransform.position.x,lerpStrength /** Time.fixedDeltaTime * baseLerpMultiplier*/);
+        targetXPos = Mathf.Lerp(transform.position.x, playerTransform.position.x,lerpStrength * Time.fixedDeltaTime * baseLerpMultiplier);
         //print("lerp strength: " + (lerpStrength * Time.fixedDeltaTime * baseLerpMultiplier));
 
         // bundle into Vector2 targetPos
         resultTargetPos.x = targetXPos;
         resultTargetPos.y = transform.position.y; // maintain same height
-        print("resultTargetPos: " + resultTargetPos);
 
         rb.MovePosition(resultTargetPos);
 
