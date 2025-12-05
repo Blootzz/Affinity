21a22ffc7e9ddf1c3fe3729dd3db4e85f7fc144d
Blootzz <106506598+Blootzz@users.noreply.github.com>
2025-12-04 20:15:28 -0600

    slomo uses fixed time and cinemachine brain ignores

--- Assets/Scripts/2.0 Utility/TimeManager.cs
+++ Assets/Scripts/2.0 Utility/TimeManager.cs
@@ -2,17 +2,27 @@ using UnityEngine;
 
 public class TimeManager : MonoBehaviour
 {
+    float initialFixedDeltaTime;
+
+    private void Start()
+    {
+        initialFixedDeltaTime = Time.fixedDeltaTime;
+    }
+
     public void SetTimeRegular()
     {
         Time.timeScale = 1;
+        Time.fixedDeltaTime = initialFixedDeltaTime;
     }
     public void SetTimeQuarter()
     {
         Time.timeScale = 0.25f;
+        Time.fixedDeltaTime = initialFixedDeltaTime / Time.timeScale;
     }
 
     public void SetTimeScale(float timeScale)
     {
         Time.timeScale = timeScale;
+        Time.fixedDeltaTime = initialFixedDeltaTime / timeScale;
     }
 }
