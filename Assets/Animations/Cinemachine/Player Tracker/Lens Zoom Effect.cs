c9112ad699b9f1973400d57e8bb5b55d3bc0d194
Blootzz <106506598+Blootzz@users.noreply.github.com>
2025-12-10 20:32:32 -0600

    Added zoom by button

---
+++ Assets/Animations/Cinemachine/Player Tracker/Lens Zoom Effect.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System;
+using Unity.Cinemachine;
+using System.Collections;
+
+public class LensZoomEffect : MonoBehaviour
+{
+    // animation will determin percent zoom starting at 1.0 and go 
+    [Tooltip("The animated ratio the camera should zoom upon a parry. 0.5 = zoomed in halfway, 2.0 = zoomed out double")]
+    public float animatedZoomRatio = 1;
+    float startingLensOrthoSize;
+
+    CinemachineCamera cineCam;
+
+    [InspectorButton(nameof(OnButtonClicked))]
+    public bool zoom;
+    private void OnButtonClicked() {
+        GetComponent<Animator>().Play("Parry Zoom", -1, 0);
+    }
+
+    private void Start()
+    {
+        cineCam = GetComponent<CinemachineCamera>();
+        startingLensOrthoSize = cineCam.Lens.OrthographicSize;
+    }
+
+    private void Update()
+    {
+        cineCam.Lens.OrthographicSize = startingLensOrthoSize * animatedZoomRatio;
+    }
+}
