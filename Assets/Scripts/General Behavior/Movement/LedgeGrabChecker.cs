9ed4c0564bc07f9bb6c86f52405a3ecb92243377
Blootzz <106506598+Blootzz@users.noreply.github.com>
2025-12-14 15:38:18 -0600

    LedgeGrabChecker optimized and outputs Vec2

--- Assets/Scripts/General Behavior/Movement/LedgeGrabChecker.cs
+++ Assets/Scripts/General Behavior/Movement/LedgeGrabChecker.cs
@@ -3,7 +3,7 @@ using System;
 
 public class LedgeGrabChecker : MonoBehaviour
 {
-    public event Action LedgeGrabEvent;
+    public event Action<Vector2> LedgeGrabEvent;
 
     [Header("Reference Point Transforms")]
     [SerializeField] Transform InnerPoint;
@@ -12,35 +12,40 @@ public class LedgeGrabChecker : MonoBehaviour
 
     [SerializeField] LayerMask layerMask;
 
-    private void OnEnable()
-    {
-        
-    }
-
+    // intended to run whenever this Gameobject is enabled
     private void FixedUpdate()
     {
-        if (EvaluateLedgeGrab())
+        if (TryEvaluateLedgeGrab(out Vector2 cornerPosition))
         {
-            print("Ledge found");
+            print("Corner found at: " + cornerPosition);
+            LedgeGrabEvent?.Invoke(cornerPosition);
         }
     }
 
-    bool EvaluateLedgeGrab()
+    /// <summary>
+    /// Uses linecasts from upper and inner points to a outer-lower point to determine if there is a corner that can be grabbed onto
+    /// </summary>
+    /// <returns>true if cornerPos is nonzero</returns>
+    bool TryEvaluateLedgeGrab(out Vector2 result)
     {
+        // only return true if both RaycastHit2D results yield a distance.
+        // If there is nothing, it measures 0. If it starts inside and object, it returns 0
+     
+        result = Vector2.zero; // default value that will never be used
+
+        // Raycasts are separated to reduce Raycast calls. Check horizontal first then vertical
         RaycastHit2D horizontalRay = Physics2D.Linecast(InnerPoint.position, TargetPoint.position, layerMask);
-        RaycastHit2D verticalRay = Physics2D.Linecast(TopPoint.position, TargetPoint.position, layerMask);
-        print("Horizontal distance: " + horizontalRay.distance);
-        print("Vertical distance: " + verticalRay.distance);
-        if (horizontalRay.distance > 0 && verticalRay.distance > 0)
+        if (horizontalRay.distance > 0)
         {
-            print("Found a corner");
-            return true;
+            RaycastHit2D verticalRay = Physics2D.Linecast(TopPoint.position, TargetPoint.position, layerMask);
+            if (verticalRay.distance > 0)
+            {
+                result = new Vector2(horizontalRay.point.x, verticalRay.point.y);
+                return true;
+            }
         }
-        else
-        {
-            print("No corner found");
-            return false;
-        }
+
+        return false;
     }
 
 }//LedgeGrab
