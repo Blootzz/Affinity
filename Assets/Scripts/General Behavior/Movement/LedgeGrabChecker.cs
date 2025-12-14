aa7a4ad9343450f78708d15aac80b9e752b01c52
Blootzz <106506598+Blootzz@users.noreply.github.com>
2025-12-14 12:31:21 -0600

    Moved LedgeGrabChecker

---
+++ Assets/Scripts/General Behavior/Movement/LedgeGrabChecker.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System;
+
+public class LedgeGrabChecker : MonoBehaviour
+{
+    public event Action LedgeGrabEvent;
+
+    BoxCollider2D detectZone;
+
+    private void Awake()
+    {
+        detectZone = GetComponent<BoxCollider2D>();
+    }
+
+
+
+}//LedgeGrab
