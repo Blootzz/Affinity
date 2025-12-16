8732d4491035f0483493c977dfdc9a0a03cdb10f
Blootzz <106506598+Blootzz@users.noreply.github.com>
2025-12-16 03:13:56 -0600

    ledge climb is jittery

--- Assets/Scripts/Player/2.0 Input and States/State Management/Individual States/PlayerStateLedgeClimb.cs
+++ Assets/Scripts/Player/2.0 Input and States/State Management/Individual States/PlayerStateLedgeClimb.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class PlayerStateLedgeClimb : PlayerBaseState
 {
+    // used to address ledge climb animation dropping the player at exactly the ledge when the animation ends farther
+    Vector2 getupOffset = new Vector2(0.325f, 0);
+
     public PlayerStateLedgeClimb(PlayerStateManager newStateManager) : base(newStateManager)
     {
     }
@@ -11,6 +14,10 @@ public class PlayerStateLedgeClimb : PlayerBaseState
         stateManager.characterMover.SetRbType(RigidbodyType2D.Kinematic);
         stateManager.characterMover.SetVelocity(Vector2.zero);
 
+        // ledge climb sprites have pivot set to where the corner ought to be, so set position to the ledge
+        // trying to turn on root motion breaks the character movement because the animator will always be trying to force the player to not let its transform move
+        stateManager.characterMover.SetPosition(stateManager.ledgeGrabPos);
+
         // animation sets playerHitbox.SetActive to true
         stateManager.playerAnimationManager.PlayAnimation(stateManager.playerAnimationManager.AorUClimbLedge);
     }
@@ -18,6 +25,8 @@ public class PlayerStateLedgeClimb : PlayerBaseState
     public override void OnExit()
     {
         stateManager.characterMover.SetRbType(RigidbodyType2D.Dynamic);
+        stateManager.characterMover.SetPosition(getupOffset * (stateManager.faceRight ? 1 : -1) + stateManager.ledgeGrabPos);
+        Debug.Log("Setting position to: " + (getupOffset * (stateManager.faceRight ? 1 : -1)) + stateManager.ledgeGrabPos);
     }
 
     public override void EndStateByAnimation()
