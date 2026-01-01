5806bd22629aa08b2b7092eea1edecdfdf54185c
Blootzz <106506598+Blootzz@users.noreply.github.com>
2026-01-01 17:31:05 -0600

    Added 7 musical mode SO

--- Assets/Scripts/Player/2.0 Input and States/Player Abilities/GuitarPlaying/Scale.cs
+++
@@ -1,7 +0,0 @@
-[System.Serializable]
-public class Scale
-{
-    public string scaleName;
-    public int[] spacings = new int[9]; // 1s and 2s for half and whole steps, respectively
-
-}
---
+++ Assets/Scripts/Player/2.0 Input and States/Player Abilities/GuitarPlaying/Scales/Scale.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Guitar/Scale")]
+public class Scale : ScriptableObject
+{
+    public string nameDesignation;
+    [Header("1 = half step, 2 = whole step")]
+    public int[] spacings = new int[9]; // 1s and 2s for half and whole steps, respectively
+}
