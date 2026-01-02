ea4f5c1b4d8de23a47d290923f56e2caccee0f3a
Blootzz <106506598+Blootzz@users.noreply.github.com>
2026-01-02 00:24:12 -0600

    sum ting wong

--- Assets/Scripts/General Behavior/Movement/GroundCheck.cs
+++ Assets/Scripts/General Behavior/Movement/GroundCheck.cs
@@ -11,6 +11,10 @@ public class GroundCheck : MonoBehaviour
     BoxCollider2D myCollider;
     List<Collider2D> overlapResults = new List<Collider2D>();
 
+    [Header("Wall Detection Points")]
+    [SerializeField] Transform pointA;
+    [SerializeField] Transform pointB;
+
     private void Awake()
     {
         myCollider = GetComponent<BoxCollider2D>();
@@ -19,19 +23,39 @@ public class GroundCheck : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         //print("entering: " + collision.name);
+
+        if (DidWeActuallyJustFindAVerticalWall())
+            return;
+
         IsGrounded = true;
         OnGroundedChanged?.Invoke(IsGrounded);
     }
+
     private void OnTriggerExit2D(Collider2D collision)
     {
         //print("exiting: " + collision.name);
 
         // evaluate if there really is nothing in GroundCheck
-        overlapResults.Clear();
-        if (myCollider.Overlap(overlapResults) != 0)
+        if (!IsGroundCheckStillOccupied())
             return;
 
         IsGrounded = false;
         OnGroundedChanged?.Invoke(IsGrounded);
+    }
+
+    bool IsGroundCheckStillOccupied()
+    {
+        overlapResults.Clear();
+        if (myCollider.Overlap(overlapResults) != 0)
+            return true;
+        return false;
+    }
+
+    bool DidWeActuallyJustFindAVerticalWall()
+    {
+        RaycastHit2D hit = Physics2D.Linecast(pointA.position, pointB.position);
+        if (hit.collider != null)
+            return true;
+        return false;
     }
 }
