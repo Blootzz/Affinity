a51191c1d56d92b4adaa8ce00b6024a0ffb9fb3f
Blootzz <106506598+Blootzz@users.noreply.github.com>
2026-01-02 00:00:38 -0600

    Increased GroundCheck width

--- Assets/Scripts/General Behavior/Movement/GroundCheck.cs
+++ Assets/Scripts/General Behavior/Movement/GroundCheck.cs
@@ -1,10 +1,20 @@
-using UnityEngine;
+using NUnit.Framework;
 using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class GroundCheck : MonoBehaviour
 {
     public event Action<bool> OnGroundedChanged; // subscribed to by PlayerStateManager.DoStateGroundedChange(bool isGrounded)
     public bool IsGrounded { get; private set; }
+
+    BoxCollider2D myCollider;
+    List<Collider2D> overlapResults = new List<Collider2D>();
+
+    private void Awake()
+    {
+        myCollider = GetComponent<BoxCollider2D>();
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -15,6 +25,12 @@ public class GroundCheck : MonoBehaviour
     private void OnTriggerExit2D(Collider2D collision)
     {
         //print("exiting: " + collision.name);
+
+        // evaluate if there really is nothing in GroundCheck
+        overlapResults.Clear();
+        if (myCollider.Overlap(overlapResults) != 0)
+            return;
+
         IsGrounded = false;
         OnGroundedChanged?.Invoke(IsGrounded);
     }
