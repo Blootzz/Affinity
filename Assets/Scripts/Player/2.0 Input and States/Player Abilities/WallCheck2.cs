3316f1ddbe71ed9e2b07ffb6f49a085ee4a796fa
Blootzz <106506598+Blootzz@users.noreply.github.com>
2026-01-02 15:41:56 -0600

    Used LayerMasks to fix wall check

--- Assets/Scripts/Player/2.0 Input and States/Player Abilities/WallCheck2.cs
+++ Assets/Scripts/Player/2.0 Input and States/Player Abilities/WallCheck2.cs
@@ -1,22 +1,31 @@
-using UnityEngine;
 using System;
+using UnityEngine;
+using static Codice.Client.Common.EventTracking.TrackFeatureUseEvent.Features.DesktopGUI.Filters;
 
 public class WallCheck2 : MonoBehaviour
 {
     public event Action<bool> OnWallCollisionChanged; // subscribed to by PlayerStateManager.DoStateGroundedChange(bool isGrounded)
     [SerializeField] bool IsInWall = false;
+    [Tooltip("Used to filter which layers should be processed by TriggerEnter/Exit")]
+    [SerializeField] LayerMask TargetLayerMask;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        //print("entering: " + collision.name);
-        IsInWall = true;
-        OnWallCollisionChanged?.Invoke(IsInWall);
+        if (((1 << collision.gameObject.layer) & TargetLayerMask) != 0)
+        {
+            ///print("entering: " + collision.name);
+            IsInWall = true;
+            OnWallCollisionChanged?.Invoke(IsInWall);
+        }
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
-        //print("exiting: " + collision.name);
-        IsInWall = false;
-        OnWallCollisionChanged?.Invoke(IsInWall);
+        if (((1 << collision.gameObject.layer) & TargetLayerMask) != 0)
+        {
+            //print("exiting: " + collision.name);
+            IsInWall = false;
+            OnWallCollisionChanged?.Invoke(IsInWall);
+        }
     }
 
     public bool GetIsInWall() => IsInWall;
