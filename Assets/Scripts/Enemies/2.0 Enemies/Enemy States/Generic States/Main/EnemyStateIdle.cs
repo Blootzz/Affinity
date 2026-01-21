d81d56d8d702fd7cb40aa307cfba0dfcb061790a
Blootzz <106506598+Blootzz@users.noreply.github.com>
2026-01-20 23:59:37 -0600

    Broke aggro logic

--- Assets/Scripts/Enemies/2.0 Enemies/Enemy States/Generic States/Main/EnemyStateIdle.cs
+++ Assets/Scripts/Enemies/2.0 Enemies/Enemy States/Generic States/Main/EnemyStateIdle.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "States/Enemy/Generic States/Main/Idle")]
 public class EnemyStateIdle : EnemyBaseState
 {
+    [Tooltip("If enabled, will switch straight to action1 when player enters attack zone")]
+    [SerializeField] bool onAggroEnterAction1 = true;
+
     /// <summary>
     /// Plays "Idle" animation if found
     /// </summary>
@@ -20,5 +23,14 @@ public class EnemyStateIdle : EnemyBaseState
     public virtual void DoNextAction()
     {
         Debug.LogWarning("No next action out of Idle");
+    }
+
+    public override void OnPlayerEnteredAttackZone()
+    {
+        base.OnPlayerEnteredAttackZone();
+        Debug.Log(stateManager.gameObject.name);
+        //Debug.Log(stateManager.stateAction1.name);
+        //if (onAggroEnterAction1)
+        //    stateManager.SwitchState(stateManager.stateAction1);
     }
 }
